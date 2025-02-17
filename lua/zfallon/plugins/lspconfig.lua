6ca172bedabeea976eec2d8d07bd578534941737
zfallon <zfallon10@gmail.com>
2025-02-16 19:30:42 -0500

    Some more lsps

--- lua/zfallon/plugins/lspconfig.lua
+++ lua/zfallon/plugins/lspconfig.lua
@@ -192,8 +192,10 @@ return {
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        eslint = {},
         svelte = {},
 
+        jsonls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
